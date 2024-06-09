c4bb0d1cd026a39df4552cf8f9bb3249550a6aff
rwiv <zc798546@gmail.com>
2024-06-09 19:11:11 +0900

    feat: add build codes

--- dist/file/file.d.ts
+++ dist/file/file.d.ts
@@ -7,3 +7,19 @@ export declare function readFile(filepath: string): Promise<string>;
 export declare function walkLines(input: ReadStream, fn: (line: string, idx: number, reader: readline.Interface) => Promise<void>): Promise<void>;
 export declare function readLines(input: ReadStream): Promise<string[]>;
 export declare function walk(dirPath: string, fn: (file: Dirent) => Promise<void>): Promise<void>;
+export interface FileInfo {
+    filename: string;
+    absPath: string;
+    isDirectory: boolean;
+    size: number;
+    atime: Date;
+    mtime: Date;
+    ctime: Date;
+    birthtime: Date;
+}
+export declare function exists(filepath: string): Promise<boolean>;
+export declare function readdir(dirPath: string): Promise<FileInfo[]>;
+export declare function readdirRecur(dirPath: string): Promise<FileInfo[]>;
+export declare function travel(baseDir: FileInfo, fn: (file: FileInfo) => void): Promise<void>;
+export declare function travelPromise(baseDir: FileInfo, fn: (file: FileInfo) => Promise<void>): Promise<void>;
+export declare function getFileInfo(basePath: string, filename: string): Promise<FileInfo>;
--- dist/file/path.d.ts
+++ dist/file/path.d.ts
@@ -2,4 +2,5 @@ export declare function getFilename(path: string): string;
 export declare function getDirPath(pathString: string, sep?: string): string;
 export declare function getExt(path: string): string;
 export declare function getRootPath(): string;
+export declare function isAbsPath(path: string): boolean;
 export declare function normalizedFilename(origin: string): string;
