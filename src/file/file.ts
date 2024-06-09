5ab9233ef0532eb81b60f21747eb54b3104798f2
rwiv <zc798546@gmail.com>
2024-06-09 19:10:53 +0900

    feat: add file, path api

--- src/file/file.ts
+++ src/file/file.ts
@@ -1,9 +1,9 @@
 import fs from "fs/promises";
-import {createReadStream, Dirent} from "node:fs";
+import {Dirent} from "node:fs";
 import readline from "readline";
 import {ReadStream} from "node:fs";
-import {Readable} from "stream";
 import path from "path";
+import {getDirPath, getFilename, isAbsPath} from "./path.js";
 
 export function readFile(filepath: string) {
   return fs.readFile(filepath, { encoding: "utf-8" });
@@ -46,5 +46,92 @@ export async function walk(
       const fullPath = path.resolve(dirPath, file.name);
       await walk(fullPath, fn);
     }
+  }
+}
+
+export interface FileInfo {
+  filename: string;
+  absPath: string;
+  isDirectory: boolean;
+  size: number;
+  atime: Date;
+  mtime: Date;
+  ctime: Date;
+  birthtime: Date;
+}
+
+export async function exists(filepath: string) {
+  try {
+    await fs.stat(filepath);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+// 비동기적으로 처리하기에 순서 보장 x
+export async function readdir(dirPath: string) {
+  const filenames= await fs.readdir(dirPath);
+  const promises = filenames.map(filename => getFileInfo(dirPath, filename));
+  return Promise.all(promises);
+}
+
+export async function readdirRecur(dirPath: string) {
+  const basePath = getDirPath(dirPath);
+  const filename = getFilename(dirPath);
+  const dirFileInfo = await getFileInfo(basePath, filename);
+
+  const result: FileInfo[] = [];
+  await travel(dirFileInfo, file => {
+    result.push(file);
+  });
+  return result;
+}
+
+export async function travel(baseDir: FileInfo, fn: (file: FileInfo) => void) {
+  fn(baseDir);
+
+  const elems = await readdir(baseDir.absPath);
+  const files = elems.filter(it => !it.isDirectory);
+  const dirs = elems.filter(it => it.isDirectory);
+
+  for (const file of files) {
+    fn(file);
+  }
+  // 비동기로 처리해버리면 순서가 엉망이 되므로 동기 처리
+  for (const dir of dirs) {
+    await travel(dir, fn);
+  }
+}
+
+export async function travelPromise(baseDir: FileInfo, fn: (file: FileInfo) => Promise<void>) {
+  await fn(baseDir);
+
+  const elems = await readdir(baseDir.absPath);
+  const files = elems.filter(it => !it.isDirectory);
+  const dirs = elems.filter(it => it.isDirectory);
+
+  await Promise.all(files.map(file => fn(file)));
+  // 이것까지 비동기로 처리해버리면 순서가 엉망이 되므로 동기 처리
+  for (const dir of dirs) {
+    await travelPromise(dir, fn);
+  }
+}
+
+export async function getFileInfo(basePath: string, filename: string): Promise<FileInfo> {
+  const absPath = path.resolve(basePath, filename);
+  if (!isAbsPath(absPath)) {
+    throw Error("is not abs path");
+  }
+  const stats = await fs.stat(absPath);
+  return {
+    filename,
+    absPath,
+    isDirectory: stats.isDirectory(),
+    size: stats.size,
+    atime: stats.atime,
+    mtime: stats.mtime,
+    ctime: stats.ctime,
+    birthtime: stats.birthtime,
   }
 }
--- src/file/path.ts
+++ src/file/path.ts
@@ -2,22 +2,42 @@ import path from "path";
 
 export function getFilename(path: string) {
   const chunks = path.split(/[\\/]/);
+  if (chunks.length === 1) {
+    return path;
+  }
   return chunks[chunks.length - 1];
 }
 
 export function getDirPath(pathString: string, sep: string = path.sep) {
   const chunks = pathString.split(sep);
+  if (chunks.length === 1) {
+    return pathString;
+  }
   chunks.pop();
   return chunks.join(sep);
 }
 
 export function getExt(path: string) {
-  const chunks = getFilename(path).split(".");
+  const filename = getFilename(path);
+  if (!filename.includes(".")) {
+    return "";
+  }
+  const chunks = filename.split(".");
   return chunks[chunks.length - 1];
 }
 
 export function getRootPath() {
   return path.resolve();
+}
+
+export function isAbsPath(path: string) {
+  if (path[0] === "/") {
+    return true;
+  } else if (path.includes(":")) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // '\/:*?"<>|' -> '＼／：＊？″＜＞｜'
